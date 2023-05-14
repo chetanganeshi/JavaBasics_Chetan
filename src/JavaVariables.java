0205d7943348211d3c5c03d8d4a49c7b5201f965
chetanganeshi <chetanganeshi1812@gmail.com>
2023-05-14 23:39:19 +0530

    Commit 1

---
+++ src/JavaVariables.java
@@ -0,0 +1,28 @@
+public class JavaVariables {
+    public static void main(String[] args) {
+        String name = "Virat Kohli";
+        System.out.println(name);
+
+        int myNum = 15;
+        System.out.println(myNum);
+
+        // Note that if you assign a new value to an existing variable, it will overwrite the previous value.
+
+        // Final Variables:
+        // If you don't want others (or yourself) to overwrite existing values, use the final keyword (this will declare the variable as "final" or "constant", which means unchangeable and read-only)
+
+         final int myFinalNum = 16;
+         System.out.println(myFinalNum);
+
+        // If you try to change 'myFinalNum' variable, it will generate error: cannot assign a value to a final variable.
+        // myFinalNum = 17;
+        // Uncomment above line to witness the error.
+
+        int myNumber = 18;
+        float myFloatNum = 15.88f;
+        char myLetter = 'c';
+        boolean myBool = true;
+        String myText = "Glenn Maxwell";
+
+    }
+}
